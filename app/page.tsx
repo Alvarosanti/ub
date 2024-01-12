4e033f7c0bd0254e3c4bc66faf3678b2f2079382
Alvarosanti <alvarosantisteban56@gmail.com>
2024-01-11 19:04:40 -0500

    2nd commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import Categories from "@/components/Categories";
+import Promotions from "@/components/Promotions";
+import Comments from "@/components/Comments";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Categories />
+      <Promotions />
+      <Comments />
+      <Contact />
+    </>
+  );
+}
