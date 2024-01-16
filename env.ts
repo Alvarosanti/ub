3104c79ff5c39e461998eb1c72a7de7416e96029
Alvarosanti <alvarosantisteban56@gmail.com>
2024-01-16 14:05:54 -0500

    upd env

--- env.ts
+++ env.ts
@@ -5,5 +5,5 @@ const env = cleanEnv(process.env,{
     VERCEL_TEMPLATE_ID: str({default:'template_qzb9lgh'}),
     VERCEL_PUBLIC_KEY:str({default:'eW9YxPtvIBR1yTBjN'})
 })
-
+//
 export default env;
