c9ee51c3df5722a441a227c60f3d40b8d360daef
Alvarosanti <alvarosantisteban56@gmail.com>
2024-01-19 13:52:46 -0500

    upd env contact prd

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -60,12 +60,12 @@ const Contact = () => {
     emailjs
       .send(
         // import.meta.env.VITE_VERCEL_SERVICE_ID,
-        env.VERCEL_SERVICE_ID,
-        // process.env.NEXT_PUBLIC_SERVICE_ID as string,
+        // env.VERCEL_SERVICE_ID,
+        process.env.NEXT_PUBLIC_SERVICE_ID as string,
 
         // import.meta.env.VITE_VERCEL_TEMPLATE_ID,
-        env.VERCEL_TEMPLATE_ID,
-        // process.env.NEXT_PUBLIC_TEMPLATE_ID as string,
+        // env.VERCEL_TEMPLATE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID as string,
 
         {
           from_name: form.name,
@@ -75,8 +75,8 @@ const Contact = () => {
           message: form.message,
         },
         // import.meta.env.VITE_VERCEL_PUBLIC_KEY
-        env.VERCEL_PUBLIC_KEY
-        // process.env.NEXT_PUBLIC_KEY
+        // env.VERCEL_PUBLIC_KEY
+        process.env.NEXT_PUBLIC_KEY
       )
       .then(
         () => {
