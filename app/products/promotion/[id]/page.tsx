379faa406acc68ef07c2f5ae0fcf1172ab8de037
Alvarosanti <alvarosantisteban56@gmail.com>
2024-01-18 19:48:08 -0500

    fix product detail & promotions

--- app/products/promotion/[id]/page.tsx
+++ app/products/promotion/[id]/page.tsx
@@ -2,14 +2,31 @@
 import Image from "next/image";
 import { promotions } from "../../../../constants/products";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface Product {
   id: number;
   nombre: string;
 }
 
+interface ButtonProps {
+  onClick: () => void;
+}
+
 const page: React.FC = () => {
   const [id, setId] = useState<string | null>(null);
+  const [talla, setTalla] = useState("");
+  const [cantidad, setCantidad] = useState("");
+  const [disabled, setDisable] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!talla || cantidad <= "") {
+      setDisable(true);
+    } else {
+      setDisable(false);
+    }
+  }, [talla, cantidad]);
 
   useEffect(() => {
     // Obtener el id de la URL usando JavaScript en el cliente
@@ -34,6 +51,10 @@ const page: React.FC = () => {
     );
   }
 
+  const setPath = () => {
+    router.push("/");
+  };
+
   return (
     <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
       <div className="mx-auto max-w-[1000px] padding-container relative w-auto flex justify-end">
@@ -49,7 +70,7 @@ const page: React.FC = () => {
               />
             </div>
           </div>
-          <div className="px-6 md:py-3 lg:w-1/2">
+          <form className="px-6 md:py-3 lg:w-1/2">
             <h1 className="font-bold text-3xl md:text-6xl mb-3 text-primary leading-tight">
               {product.title}
             </h1>
@@ -72,24 +93,22 @@ const page: React.FC = () => {
                   </label>
                   <div className="relative">
                     <select
-                      id="style"
-                      name="style"
-                      value={""}
-                      className="block appearance-none w-full bg-gainsboro border-2 border-gainsboro focus:border-slategray px-4 py-3 pr-8 focus:outline-none focus:bg-white text-slategray focus:text-slategray rounded-lg"
-                      // onChange=()
+                      id="talla"
+                      name="talla"
+                      value={talla}
+                      className="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray- dark:border-gray-200 placeholder-gray-100 dark:text-black dark:focus:ring-blue-300 dark:focus:border-blue-300"
+                      onChange={(e) => {
+                        setTalla(e.target.value);
+                      }}
+                      required
                     >
+                      <option value="">Seleccionar...</option>
                       {product.sizes.map((talla) => (
                         <option key={talla} value={talla}>
                           {talla}
                         </option>
                       ))}
                     </select>
-                    <div className="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
-                      {/* <ChevronDownSmallIcon
-                    className="h-4 w-4 text-gray-400"
-                    aria-hidden="true"
-                  /> */}
-                    </div>
                   </div>
                 </div>
               ) : null}
@@ -98,16 +117,20 @@ const page: React.FC = () => {
                   className="block text-sm font-bold tracking-widest uppercase mb-2 text-slategray"
                   htmlFor="quantity"
                 >
-                  Quantity
+                  Cantidad
                 </label>
                 <div className="relative">
                   <select
-                    id="quantity"
-                    name="quantity"
-                    value={""}
-                    className="block appearance-none w-full bg-gainsboro border-2 border-gainsboro focus:border-slategray px-4 py-3 pr-8 focus:outline-none focus:bg-white text-slategray focus:text-slategray rounded-lg"
-                    // onChange={updateQuantity}
+                    id="cantidad"
+                    name="cantidad"
+                    value={cantidad}
+                    className="bg-gray-10 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray- dark:border-gray-200 placeholder-gray-100 dark:text-black dark:focus:ring-blue-300 dark:focus:border-blue-300"
+                    onChange={(e) => {
+                      setCantidad(e.target.value);
+                    }}
+                    required
                   >
+                    <option value="">Seleccionar...</option>
                     {Array.from({ length: 5 }, (_, i) => {
                       const value = Number(i + 1);
 
@@ -118,19 +141,31 @@ const page: React.FC = () => {
                       );
                     })}
                   </select>
-                  {/* <div className="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
-                <ChevronDownSmallIcon
-                  className="h-4 w-4 text-gray-400"
-                  aria-hidden="true"
-                />
-              </div> */}
                 </div>
               </div>
             </div>
-            <button>Add to cart</button>
-
-            {/* <ProductReviews product={product} /> */}
-          </div>
+            <div>
+              {disabled ? (
+                <button
+                  type="submit"
+                  rel="noopener noreferrer"
+                  className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded inline-block"
+                >
+                  Comprar
+                </button>
+              ) : (
+                <a
+                  onClick={setPath}
+                  href={`https://wa.me/987654321?text=Hola que tal, quiero comprar el producto: ${product.title} cuesta S/.${product.price} c/u, quiero ${cantidad} unidades, en talla ${talla}.`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded inline-block"
+                >
+                  Comprar
+                </a>
+              )}
+            </div>
+          </form>
         </div>
       </div>
     </section>
