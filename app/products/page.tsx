5acda4ee73d241cca07668e79575d3b53249f159
Alvarosanti <alvarosantisteban56@gmail.com>
2024-01-22 21:16:00 -0500

    responsive product section

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -11,7 +11,7 @@ const products = () => {
     <div>
       <Banner />
       <div className="max-w-6xl mx-auto mt-10 px-4">
-        <Arrowback link="/#categories"/>
+        <Arrowback link="/#categories" />
         <div className="py-8 border-b border-slate-200 space-y-4">
           <h1 className="text-5xl font-semibold">Productos</h1>
           <p className="text-slate-700 max-w-3xl">
@@ -20,9 +20,11 @@ const products = () => {
             armonía.
           </p>
         </div>
-        <div className="grid grid-cols-8 py-8 gap-10">
-          <FilterSection />
-          <div className="col-span-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 py-8 gap-10">
+          <div className="md:col-span-1 lg:col-span-2 xl:col-span-1 2xl:col-span-1">
+            <FilterSection />
+          </div>
+          <div className="md:col-span-1 lg:col-span-3 xl:col-span-3 2xl:col-span-4">
             <ProductSection />
           </div>
         </div>
